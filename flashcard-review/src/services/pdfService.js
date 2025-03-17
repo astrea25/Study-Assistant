94aadcd524c223ef1713dbc357a2854a384c7f98
astermangabat25 <157344930+astermangabat25@users.noreply.github.com>
2025-03-17 21:14:20 +0800

    updated pdfservice for deployment

--- flashcard-review/src/services/pdfService.js
+++ flashcard-review/src/services/pdfService.js
@@ -5,7 +5,7 @@ class PDFService {
       formData.append('pdfFile', file);
   
       try {
-        const API_URL = "https://pdf-reader-neon.vercel.app/" || "http://localhost:5000";
+        const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
         const response = await fetch(`${API_URL}/extract-text`, {
           method: 'POST',
